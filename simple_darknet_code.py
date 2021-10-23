c300f5373a2311cdfdd07c542b754b868985c0f2
Utkarsh2401 <84897510+Utkarsh2401@users.noreply.github.com>
2021-10-23 16:00:14 +0530

    Update simple_darknet_code.py

--- simple_darknet_code.py
+++ simple_darknet_code.py
@@ -1,3 +1,4 @@
+from ctypes import resize
 import numpy as np
 import darknet
 import cv2
@@ -9,27 +10,23 @@ darknet_height = darknet.network_height(network)
 
 input_path = input("Enter input path: ")
 if input_path:
-    cap = cv2.VideoCapture(input_path)
-else:
-    cap = cv2.VideoCapture(0)
+    frame = cv2.imread(input_path)
 
-while (cap.isOpened()):
-
-    _,frame = cap.read()
+while True:
     
     frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    frame_resized = cv2.resize(frame_rgb, (darknet_width, darknet_height), interpolation=cv2.INTER_LINEAR)
-    img_for_detect = darknet.make_image(darknet_width, darknet_height, 3)
+    frame_resized = cv2.resize(frame_rgb, (darknet_height, darknet_width), interpolation=cv2.INTER_LINEAR)
+    img_for_detect = darknet.make_image(darknet_height, darknet_width, 3)
     darknet.copy_image_from_bytes(img_for_detect, frame_resized.tobytes())
 
     detection = darknet.detect_image(network, class_names, img_for_detect, thresh = 0.25)
     darknet.print_detections(detection, True)
 
-    image = darknet.draw_boxes(detection, frame, class_colors)
-    cv2.imshow('Inference', image)
+    image = darknet.draw_boxes(detection, frame_resized, class_colors)
+    image_bgr = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    cv2.imshow('Inference', image_bgr)
 
-    cv2.waitKey(10)
+    if cv2.waitKey() & 0xFF == ord('q'):
+        break
 cv2.destroyAllWindows()
-cap.release()
-       
 
